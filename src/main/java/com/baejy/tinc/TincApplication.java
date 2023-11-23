b797633c5d8ca9a33417455bd7062d5179de5453
baejymil <baejymil@gmail.com>
2023-11-24 00:54:00 +0900

    Initial commit

---
+++ src/main/java/com/baejy/tinc/TincApplication.java
@@ -0,0 +1,13 @@
+package com.baejy.tinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TincApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TincApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baejy/tinc/TincApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baejy.tinc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TincApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
